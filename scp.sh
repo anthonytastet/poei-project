9fb7107fb3d7553ee028311f53d808de8addcef5
anthony <anthonyd.tastet@gmail.com>
2023-03-09 11:21:11 +0100

    Add scp.sh file

---
+++ scp.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/trainee/poei-project/
+
+scp trainee@79.137.68.149:/home/trainee/poei-project/ trainee@10.0.2.45:/home/trainee/
+scp trainee@79.137.68.149:/home/trainee/poei-project/ trainee@10.0.2.46:/home/trainee/
