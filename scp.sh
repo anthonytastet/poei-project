72e11593f13810ccce9b44c836acea2c8238ba48
anthony <anthonyd.tastet@gmail.com>
2023-03-09 12:05:49 +0100

    Edit scp.sh

--- scp.sh
+++ scp.sh
@@ -1,4 +1,2 @@
-scp -r /home/trainee/poei-project/container1/* trainee@10.0.2.45:/home/trainee/
-scp -r /home/trainee/poei-project/container2/* trainee@10.0.2.45:/home/trainee/
-scp -r /home/trainee/poei-project/container3/* trainee@10.0.2.46:/home/trainee/
-scp -r /home/trainee/poei-project/container4/* trainee@10.0.2.46:/home/trainee/
+scp -r /home/trainee/poei-project/vm1/* trainee@10.0.2.45:/home/trainee/
+scp -r /home/trainee/poei-project/vm2/* trainee@10.0.2.46:/home/trainee/
